7ac94f3a35a5efecabaad02dc75d688f2d1de122
Xotic99 <landonlipe3@gmail.com>
2023-08-15 16:00:14 -0400

    all files

--- src/main/java/net/kishiyami/tutorialmod/TutorialMod.java
+++ src/main/java/net/kishiyami/tutorialmod/TutorialMod.java
@@ -2,6 +2,9 @@ package net.kishiyami.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kishiyami.tutorialmod.block.ModBlocks;
+import net.kishiyami.tutorialmod.item.ModItemGroups;
+import net.kishiyami.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +14,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
